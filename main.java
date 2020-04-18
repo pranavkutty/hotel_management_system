e539f125f1c147537011a9b08df4c32ce9fd4550
pranavkutty <pranavkutty13@gmail.com>
2020-04-18 19:46:26 +0530

    done with admin features to view guests

--- main.java
+++ main.java
@@ -18,6 +18,7 @@ class Hotel{
 			Scanner sc = new Scanner(System.in);
 			while(choice1 != 3)
 			{
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 				int admin_choice=0;
 				System.out.println(":::::::::::::::Welcome to Ustad Hotel:::::::::::::::");
 				System.out.println(":::::::::::::::::::Login Portal:::::::::::::::::::::");
@@ -44,11 +45,12 @@ class Hotel{
 							if(user.equals("admin") && pwd.equals("admin"))
 							{
 								new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();				
-								System.out.println("Admin login successful");
+								System.out.println("Admin login successful!");
 								admin_choice = admin(sc,stmt);
 							}
 							else
 							{
+								new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 								System.out.println("Wrong credentials! Try again!");
 							}
 						}	
@@ -77,55 +79,98 @@ class Hotel{
 	}
 
 
-	static int admin(Scanner sc,Statement stmt)
+	static int admin(Scanner sc,Statement stmt) throws IOException, InterruptedException
 	{
 		int admin_choice=0;
-		while(admin_choice!=4)
+		try
 		{
-			System.out.println("Admin Main Menu: ");
-			System.out.println("1. View all Bookings");
-			System.out.println("2. Get details of a particular guest");
-			System.out.println("3. Free rooms");
-			System.out.println("4. Logout");
-			System.out.print("Enter your choice (1-3):");
-			admin_choice = sc.nextInt();
-			
-			switch(admin_choice)
+			while(admin_choice!=4)
 			{
-				case 1:
+				System.out.println(":::::::::::::Admin Main Menu:::::::::::::: ");
+				System.out.println("1.	View all Bookings");
+				System.out.println("2.	Get details of a particular guest");
+				System.out.println("3.	Free rooms");
+				System.out.println("4.	Logout");
+				System.out.print("Enter your choice (1-4):");
+				admin_choice = sc.nextInt();
+				
+				switch(admin_choice)
 				{
-					ResultSet rset = stmt.executeQuery("select * from hotel");
-					System.out.println("Bookings:");
-					System.out.println("+------------+--------+------------+------------+------------+----------------+---------+");
-					System.out.println("| BOOKING ID | NAME \t| CHECK IN   | CHECK OUT  | MOBILE NO  | PAYMENT STATUS | ROOM_NO |");
-					System.out.println("+------------+--------+------------+------------+------------+----------------+---------+");
-					int rowcount = 0;
-					while(rset.next())
+					case 1:
 					{
-						System.out.format("| %11d| ",rset.getInt("booking_id"));
-						++rowcount;
-
+						new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();						
+						ResultSet rset = stmt.executeQuery("select * from hotel");
+						System.out.println("Bookings:");
+						System.out.println("+------------+-------------+------------+------------+------------+----------------+---------+");
+						System.out.println("| BOOKING ID | NAME        | CHECK IN   | CHECK OUT  | MOBILE NO  | PAYMENT STATUS | ROOM_NO |");
+						System.out.println("+------------+-------------+------------+------------+------------+----------------+---------+");
+						int rowcount = 0;
+						while(rset.next())
+						{
+							System.out.format("| %11d",rset.getInt("booking_id"));
+							System.out.format("| %12s",rset.getString("name"));
+							System.out.format("| %11s",rset.getString("check_in"));
+							System.out.format("| %11s",rset.getString("check_out"));
+							System.out.format("| %11s",rset.getString("mobile_no"));
+							System.out.format("| %15s",rset.getString("payment_status"));
+							System.out.format("| %8d|",rset.getInt("room_no"));
+							System.out.println();
+							++rowcount;
+						}
+						System.out.println("+------------+-------------+------------+------------+------------+----------------+---------+");
+						System.out.println();
+						System.out.println("Press Enter to continue....");
+						sc.nextLine();
+						sc.nextLine();
+						new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+						break;
 					}
-					break;
-				}
-				case 2:
-				{
-					System.out.print("Enter RoomNo: ");
-					//select and display for bookingid
-					break;
-				}
-				case 3:
-				{
-					//free rooms
-					break;
-				}
-				case 4:
-				{
-					//new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-					System.out.println("Logged out successfully");
-					break;
+					case 2:
+					{
+						new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+						System.out.print("Enter Booking ID: ");
+						int booking_id = sc.nextInt();
+						System.out.println("The details of booking ID "+booking_id+" is:");
+						ResultSet rset = stmt.executeQuery("select * from hotel where booking_id = "+booking_id);
+						System.out.println("+------------+-------------+------------+------------+------------+----------------+---------+");
+						System.out.println("| BOOKING ID | NAME        | CHECK IN   | CHECK OUT  | MOBILE NO  | PAYMENT STATUS | ROOM_NO |");
+						System.out.println("+------------+-------------+------------+------------+------------+----------------+---------+");
+						int rowcount = 0;
+						while(rset.next())
+						{
+							System.out.format("| %11d",rset.getInt("booking_id"));
+							System.out.format("| %12s",rset.getString("name"));
+							System.out.format("| %11s",rset.getString("check_in"));
+							System.out.format("| %11s",rset.getString("check_out"));
+							System.out.format("| %11s",rset.getString("mobile_no"));
+							System.out.format("| %15s",rset.getString("payment_status"));
+							System.out.format("| %8d|",rset.getInt("room_no"));
+							System.out.println();
+							++rowcount;
+						}
+						System.out.println("+------------+-------------+------------+------------+------------+----------------+---------+");
+						System.out.println();
+						System.out.println("Press Enter to continue....");
+						sc.nextLine();
+						sc.nextLine();
+						new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+						break;
+					}
+					case 3:
+					{
+						//free rooms
+						break;
+					}
+					case 4:
+					{
+						new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+						System.out.println("Logged out successfully");
+						break;
+					}
 				}
 			}
+		}catch(SQLException e){
+			e.printStackTrace();
 		}
 		return admin_choice;
 	}
